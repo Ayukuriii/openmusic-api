9f2089a89a3f0a19bb9156b42d162cdf6d123605
Ekadian Haris <ekadianharis@gmail.com>
2024-01-02 06:04:47 +0700

    AlbumService for db process done

--- src/services/postgres/AlbumService.js
+++ src/services/postgres/AlbumService.js
@@ -2,6 +2,7 @@ const { nanoid } = require('nanoid')
 const { Pool } = require('pg')
 const InvariantError = require('../../exception/InvariantError')
 const { mapDBToModel } = require('../../utils/albums')
+const NotFoundError = require('../../exception/NotFoundError')
 
 class AlbumService {
   constructor() {
@@ -21,7 +22,7 @@ class AlbumService {
     const result = await this._pool.query(query)
 
     if (!result.rows[0].id) {
-      throw new InvariantError('Catatan gagal ditambahkan')
+      throw new InvariantError('Album gagal ditambahkan')
     }
 
     return result.rows[0].id
@@ -31,6 +32,49 @@ class AlbumService {
     const result = await this._pool.query('SELECT * FROM albums')
 
     return result.rows.map(mapDBToModel)
+  }
+
+  async getAlbumById(id) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id],
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Album tidak ditemukan')
+    }
+
+    return result.rows.map(mapDBToModel)[0]
+  }
+
+  async editAlbumById(id, { title, year }) {
+    const updatedAt = new Date().toISOString()
+
+    const query = {
+      text: 'UPDATE albums SET title = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id',
+      values: [title, year, updatedAt, id],
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui album. Id tidak ditemukan')
+    }
+  }
+
+  async deleteAlbumById(id) {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+      values: [id],
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan.')
+    }
   }
 }
 
