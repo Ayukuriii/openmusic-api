19667b102637c1d9c924de28719747d1e9226f49
Ekadian Haris <ekadianharis@gmail.com>
2024-01-02 05:02:33 +0700

    init open music api - dicoding

---
+++ src/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  })
+
+  await server.register({})
+
+  server.ext()
+
+  await server.start()
+  console.log(`Server berjalan pada ${server.info.uri}`)
+}
+
+init()
