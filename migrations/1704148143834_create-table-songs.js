1a0b13e79a1866f716f5bae9abf2941b0f2bebb5
Ekadian Haris <ekadianharis@gmail.com>
2024-01-09 21:38:46 +0700

    fix foreignkey | openmusic v2 done

--- migrations/1704148143834_create-table-songs.js
+++ migrations/1704148143834_create-table-songs.js
@@ -37,12 +37,6 @@ exports.up = (pgm) => {
       notNull: true,
     },
   })
-
-  pgm.addConstraint(
-    'songs',
-    'fk_songs.album_id_songs.id',
-    'FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE'
-  )
 }
 
 exports.down = (pgm) => {
