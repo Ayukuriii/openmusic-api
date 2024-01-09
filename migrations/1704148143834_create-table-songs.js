2eadd29adb7650a178d0d8f163e4633ebb75ac4e
Ekadian Haris <ekadianharis@gmail.com>
2024-01-09 21:34:54 +0700

    fix foreignkey on songs

--- migrations/1704148143834_create-table-songs.js
+++ migrations/1704148143834_create-table-songs.js
@@ -37,6 +37,12 @@ exports.up = (pgm) => {
       notNull: true,
     },
   })
+
+  pgm.addConstraint(
+    'songs',
+    'fk_songs.album_id_songs.id',
+    'FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE'
+  )
 }
 
 exports.down = (pgm) => {
